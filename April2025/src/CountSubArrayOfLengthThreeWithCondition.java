a522feee3298527c318538ec2bee4c28a05044c1
kunwaraman <amankunwar8472@gmail.com>
2025-04-27 12:34:00 +0530

    day27

---
+++ April2025/src/CountSubArrayOfLengthThreeWithCondition.java
@@ -0,0 +1,16 @@
+/*
+time complexity: O(n)
+space complexity: O(1)
+ */
+public class CountSubArrayOfLengthThreeWithCondition {
+    public int countSubarrays(int[] nums) {
+        int n = nums.length;
+        int count=0;
+        for(int i=0;i<n-2;i++){
+            if(2*(nums[i]+nums[i+2])==nums[i+1]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
