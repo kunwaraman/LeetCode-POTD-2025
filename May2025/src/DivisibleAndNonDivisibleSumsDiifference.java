a131b1472761f3557e7b898b72d53afa3da1f9c0
kunwaraman <amankunwar8472@gmail.com>
2025-05-27 15:05:58 +0530

    day27

---
+++ May2025/src/DivisibleAndNonDivisibleSumsDiifference.java
@@ -0,0 +1,19 @@
+/*
+time complexity: O(n)
+space complexity: O(1)
+ */
+
+public class DivisibleAndNonDivisibleSumsDiifference {
+    public int differenceOfSums(int n, int m) {
+        int sum1=0;
+        int sum2=0;
+        for(int i=1;i<=n;i++){
+            if(i%m!=0){
+                sum1+=i;
+            }else if(i%m==0){
+                sum2+=i;
+            }
+        }
+        return sum1-sum2;
+    }
+}
