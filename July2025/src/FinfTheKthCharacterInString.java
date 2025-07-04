1176a30479013ecc5c96b4d44090d4821c51dea3
kunwaraman <amankunwar8472@gmail.com>
2025-07-04 09:13:36 +0530

    to prev

--- July2025/src/FinfTheKthCharacterInString.java
+++ July2025/src/FinfTheKthCharacterInString.java
@@ -1,2 +1,11 @@
+/*
+time complexity: O(1)
+space complexity: O(1)
+ */
+
 public class FinfTheKthCharacterInString {
+    public char kthCharacter(int k) {
+        int shift=Integer.bitCount(k-1);
+        return (char) ('a'+shift);
+    }
 }
