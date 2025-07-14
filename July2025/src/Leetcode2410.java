c7d86a8c9767894e0ce80544e911908e04c63ff5
kunwaraman <amankunwar8472@gmail.com>
2025-07-14 18:10:41 +0530

    pv

--- July2025/src/Leetcode2410.java
+++ July2025/src/Leetcode2410.java
@@ -1,2 +1,28 @@
+import java.util.*;
+/*
+time complexity: O(nlogn)+O(mlogm)
+space complexity: O(1)
+ */
+
+
 public class Leetcode2410 {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int n = players.length;
+        int m = trainers.length;
+        int i=0;
+        int j=0;
+        int count=0;
+        while(i<n && j<m){
+            if(players[i]<=trainers[j]){
+                count++;
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return count;
+    }
 }
