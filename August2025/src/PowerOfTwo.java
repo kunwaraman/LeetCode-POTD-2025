0f07f3feeb555ea8d806525d39ba1e3f95ba7ee9
kunwaraman <amankunwar8472@gmail.com>
2025-08-09 10:54:18 +0530

    day09

---
+++ August2025/src/PowerOfTwo.java
@@ -0,0 +1,13 @@
+*/*
+time complexity: O(1)
+space complexity: O(1)
+ */
+
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        return (n&(n-1))==0;
+    }
+}
