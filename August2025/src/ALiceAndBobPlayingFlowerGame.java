397ec01c3f37d4bdeb860af91edd9763fed2d703
kunwaraman <amankunwar8472@gmail.com>
2025-08-29 19:46:10 +0530

    chtg

--- August2025/src/ALiceAndBobPlayingFlowerGame.java
+++ August2025/src/ALiceAndBobPlayingFlowerGame.java
@@ -1,2 +1,15 @@
+/*
+time complexity: O(1)
+space complexity: O(1)
+ */
+
 public class ALiceAndBobPlayingFlowerGame {
+    public long flowerGame(int n, int m) {
+        long oddn=(n+1)/2;
+        long evenn=n/2;
+        long oddm=(m+1)/2;
+        long evenm=m/2;
+        return (oddn*evenm)+(oddm*evenn);
+
+    }
 }
